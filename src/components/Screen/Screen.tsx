14209a866187e8650123b67eca9b8d34bdd7168f
iForced <iforce@2ch.hk>
2021-08-23 21:40:36 +0300

    refactor styles for screen

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -6,13 +6,13 @@ type ScreenPropsType = {
 
 export const Screen: React.FC<ScreenPropsType> = ({counterValue}) => {
     const screenStyles = {
-        'border-radius': '10px',
+        borderRadius: '10px',
         padding: '100px 200px',
-        'background-color': '#F5DF4D',
-        'font-size': '9rem',
-        'font-weight': 'bold',
+        backgroundColor: '#F5DF4D',
+        fontSize: '9rem',
+        fontWeight: 900,
         color: counterValue === 5 ? 'red' : '#939597',
-        'box-shadow': 'inset 0 0 8px 2px #939597',
+        boxShadow: 'inset 0 0 8px 2px #939597',
     }
     return (
         <div style={screenStyles}>
